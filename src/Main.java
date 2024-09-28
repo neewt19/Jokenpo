32a5add5a568196a1c9ebe7eed642cc371ba0029
neewt19 <newthonaraujo19@gmail.com>
2024-09-27 21:23:01 -0300

    primeiro commit

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+import java.util.Locale;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Valor aleatório da máquina :)
+        Scanner sc = new Scanner(System.in);
+        String[] Maquina = {"Pedra", "Papel", "Tesoura"};
+        Random rand = new Random();
+        int indice = rand.nextInt(Maquina.length);
+        String maquina = Maquina[indice];
+        System.out.println(maquina);
+        //valor escolhido pelo player
+        System.out.println("Escolha um desses");
+        System.out.println("Pedra - Papel - Tesoura");
+        String escolha = sc.nextLine();
+        //verificação
+        if (maquina.equals(escolha)){
+            System.out.printf("A máquina escolheu %s", maquina);
+            System.out.println("\nEmpate");
+        }
+        if (maquina == "Pedra" && escolha == "Tesoura") {
+            System.out.printf("A máquina escolheu %s", maquina);
+            System.out.println("Vitória da máquina");
+        } else if (maquina == "Papel" && escolha == "Pedra") {
+            System.out.printf("A máquina escolheu %s", maquina);
+            System.out.println("Vitória da máquina");
+        }else if (maquina.equals("Tesoura") && escolha.toLowerCase().equals("papel")) {
+            System.out.printf("A máquina escolheu %s", maquina);
+            System.out.println("\nVitória da máquina");
+        } else {
+            System.out.printf("A máquina escolheu %s", maquina);
+            System.out.println("\nVocê Ganhou");
+        }
+        sc.close();
+    }
+}
